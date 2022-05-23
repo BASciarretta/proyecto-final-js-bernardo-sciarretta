8e1e533ca48a278601b3d4e2df750aa95e4312c8
Bernardo Sciarretta <basciarretta@hotmail.com>
2022-05-23 12:34:21 -0300

    arreglo un error

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const container = document.getElementById("container");
 
 fetch("https://pokeapi.co/api/v2/pokemon").then((response) => {
     return response.json();
-}).then((pokemons) => {
+}).then((nombrePokemons) => {
 
-    pokemons.results.forEach((pokemon) => {
+    nombrePokemons.results.forEach((pokemon) => {
         const ul = document.createElement("ul");
 
         const li = document.createElement("li");
